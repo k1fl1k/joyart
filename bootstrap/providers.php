c03e1a4f77e7717ae97810e8f4b1a131116bd2ac
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-02-20 19:20:44 +0200

    New providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     k1fl1k\joyart\Providers\AppServiceProvider::class,
     k1fl1k\joyart\Providers\MigrationServiceProvider::class,
+    k1fl1k\joyart\Providers\SafebooruServiceProvider::class,
     k1fl1k\joyart\Providers\VoltServiceProvider::class,
 ];
