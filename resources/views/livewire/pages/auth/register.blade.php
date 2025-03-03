842d8360020a6ff86c0aa0264fe4f71b5b0abe9c
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-03-03 14:44:33 +0200

    Added authorization & authentication pages, navigation and user information on top off main pages.

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -40,8 +40,6 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="register">
         <div> <!-- This will be the single root element -->
 
-                    <h2 class="text-3xl font-bold text-center text-blue-400">Register</h2>
-
                     <form wire:submit="register" class="mt-6">
                         <!-- Name -->
                         <div class="mb-4">
@@ -88,10 +86,7 @@ new #[Layout('layouts.guest')] class extends Component
                         </div>
 
                         <div class="flex justify-between items-center">
-                            <a class="text-sm text-blue-400 hover:underline" href="{{ route('login') }}" wire:navigate>
-                                Already registered?
-                            </a>
-                            <button type="submit" class="px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-400">
+                            <button type="submit" class="register-form-button">
                                 Register
                             </button>
                         </div>
