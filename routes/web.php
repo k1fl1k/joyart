cc2ce14f2292b9cb4be511480902481054166195
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-03-14 14:01:36 +0200

    Added settings route.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use k1fl1k\joyart\Http\Controllers\WelcomeController;
+use k1fl1k\joyart\Http\Controllers\WebController;
 
-Route::get('/', [WelcomeController::class, 'showWelcome'])->name('welcome');
+Route::get('/', [WebController::class, 'showWelcome'])->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('profile', 'profile')
+Route::get('profile', [WebController::class, 'showProfile'])
     ->middleware(['auth'])
     ->name('profile');
+
+Route::view('/profile/settings', "settings")
+    ->middleware(['auth'])
+    ->name('settings');
 
 require __DIR__.'/auth.php';
