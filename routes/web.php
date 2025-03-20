c7a1087a77d16e1141e9023fdf206216551141c6
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-03-20 15:49:21 +0200

    Added new route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use k1fl1k\joyart\Http\Controllers\ArtworkController;
+use k1fl1k\joyart\Http\Controllers\GalleryController;
 use k1fl1k\joyart\Http\Controllers\WebController;
+
 
 Route::get('/', [WebController::class, 'showWelcome'])->name('welcome');
 
@@ -16,5 +19,9 @@ Route::get('profile', [WebController::class, 'showProfile'])
 Route::view('/profile/settings', "settings")
     ->middleware(['auth'])
     ->name('settings');
+
+Route::get('/tag/{tag:slug}', [GalleryController::class, 'filterByTag'])->name('gallery.byTag');
+
+Route::get('/artwork/{artwork:slug}', [ArtworkController::class, 'show'])->name('artwork.show');
 
 require __DIR__.'/auth.php';
