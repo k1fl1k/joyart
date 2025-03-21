6b28d8f177b3961cd8970a9e05b9e560145dcde6
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-03-21 18:52:07 +0200

    Added likes and comments post routes.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use k1fl1k\joyart\Http\Controllers\ArtworkController;
+use k1fl1k\joyart\Http\Controllers\CommentsController;
 use k1fl1k\joyart\Http\Controllers\GalleryController;
+use k1fl1k\joyart\Http\Controllers\LikesController;
 use k1fl1k\joyart\Http\Controllers\WebController;
-
 
 Route::get('/', [WebController::class, 'showWelcome'])->name('welcome');
 
@@ -16,12 +17,16 @@ Route::get('profile', [WebController::class, 'showProfile'])
     ->middleware(['auth'])
     ->name('profile');
 
-Route::view('/profile/settings', "settings")
+Route::view('/profile/settings', 'settings')
     ->middleware(['auth'])
     ->name('settings');
 
 Route::get('/tag/{tag:slug}', [GalleryController::class, 'filterByTag'])->name('gallery.byTag');
 
 Route::get('/artwork/{artwork:slug}', [ArtworkController::class, 'show'])->name('artwork.show');
+Route::post('/artworks/{artwork:slug}/likes', [LikesController::class, 'toggle'])->name('likes.toggle')
+    ->middleware(['auth']);
+Route::post('/artworks/{artwork:slug}/comments', [CommentsController::class, 'store'])->name('comments.store')
+    ->middleware(['auth']);
 
 require __DIR__.'/auth.php';
