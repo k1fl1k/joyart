6b9cbfa9d9ba82813ab8834a82ba378e92a630fa
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-03-27 21:03:27 +0200

    Added func to work with table.

--- app/Models/Favorites.php
+++ app/Models/Favorites.php
@@ -9,4 +9,21 @@ class Favorites extends Model
 {
     /** @use HasFactory<\Database\Factories\FavoritesFactory> */
     use HasFactory;
+
+    protected $table = 'favorites';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['id', 'user_id', 'artwork_id'];
+
+    public function artwork()
+    {
+        return $this->belongsTo(Artwork::class, 'artwork_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
