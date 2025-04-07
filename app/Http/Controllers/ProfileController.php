6d5ea7c987e7a2244b514645f370719961dce573
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-04-07 14:03:53 +0300

    Updated redirecting methods.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,9 +4,22 @@ namespace k1fl1k\joyart\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use k1fl1k\joyart\Models\Artwork;
+use k1fl1k\joyart\Models\User;
 
 class ProfileController extends Controller
 {
+    public function show($username)
+    {
+        $user = User::where('username', $username)->firstOrFail();
+        $userPosts = $user->artworks;
+        $likedPosts = Artwork::whereHas('favorites', function ($query) use ($user) {
+            $query->where('user_id', $user->id);
+        })->orderBy('created_at', 'desc')->get();
+
+        return view('profile', compact('user', 'userPosts', 'likedPosts'));
+    }
+
     public function startEditing(Request $request)
     {
         // Встановлюємо сесію для редагування опису
@@ -31,5 +44,10 @@ class ProfileController extends Controller
         $request->session()->forget('editing_description');
 
         return redirect()->back()->with('success', 'Description updated successfully.');
+    }
+
+    public function settings()
+    {
+        return view('settings');
     }
 }
