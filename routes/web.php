43fbfa65566dab2457270ef8ddafae34bb78e4a7
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-04-07 14:29:17 +0300

    Added new routes. Added one console command. Changed middleware in auth.

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ use k1fl1k\joyart\Http\Controllers\ProfileController;
 use k1fl1k\joyart\Http\Controllers\WebController;
 use k1fl1k\joyart\Models\Tag;
 
-Route::get('/', [WebController::class, 'showWelcome'])->name('welcome');
-Route::get('/', [GalleryController::class, 'search'])->name('welcome');
-
+Route::get('/', [WebController::class, 'index'])->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -22,9 +20,10 @@ Route::view('dashboard', 'dashboard')
 Route::get('profile', [WebController::class, 'showProfile'])
     ->middleware(['auth'])
     ->name('profile');
-Route::view('/profile/settings', 'settings')
+Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
+Route::get('settings', [ProfileController::class, 'settings'])
     ->middleware(['auth'])
-    ->name('settings');
+    ->name('settings.show');
 Route::put('/profile/update-description', [ProfileController::class, 'updateDescription'])
     ->middleware(['auth'])
     ->name('profile.updateDescription');
@@ -36,7 +35,7 @@ Route::get('/tag/{tag:slug}', [GalleryController::class, 'filterByTag'])->name('
 
 Route::get('/create', function () {
     return view('create');
-})->name('create.post')->middleware(['auth']);;
+})->middleware(['auth'])->name('create.post');
 Route::get('/tags/search', function (Request $request) {
     $query = $request->query('query');
 
@@ -53,7 +52,7 @@ Route::get('/tags/search', function (Request $request) {
 });
 Route::post('/artworks', [ArtworkController::class, 'store'])->name('artworks.store');
 
-Route::get('/artwork/{artwork:slug}', [ArtworkController::class, 'show'])->name('artwork.show');
+Route::get('/artworks/{artwork:slug}', [ArtworkController::class, 'show'])->name('artwork.show');
 Route::post('/artworks/{artwork:slug}/likes', [LikesController::class, 'toggle'])->name('likes.toggle')
     ->middleware(['auth']);
 Route::post('/artworks/{artwork:slug}/favorites', [FavoritesController::class, 'toggle'])->name('favorites.toggle')
@@ -63,5 +62,15 @@ Route::post('/artworks/{artwork:slug}/comments', [CommentsController::class, 'st
 Route::delete('/artworks/{artwork:slug}/comments/{comment}', [CommentsController::class, 'destroy'])
     ->name('comments.destroy')
     ->middleware(['auth']);
+Route::delete('/artworks/{artwork:slug}', [ArtworkController::class, 'destroy'])->name('artworks.destroy');
+
+
+Route::get('/artworks/{artwork:slug}/edit', [ArtworkController::class, 'edit'])
+    ->middleware(['auth'])
+    ->name('artworks.edit');
+Route::put('/artworks/{artwork:slug}/edited', [ArtworkController::class, 'update'])
+    ->middleware(['auth'])
+    ->name('artworks.update');
+
 
 require __DIR__.'/auth.php';
