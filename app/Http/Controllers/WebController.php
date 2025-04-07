719218d33787d1969600240076c8bea67a5fea98
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-04-07 14:04:56 +0300

    Added filtering logic.

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,17 +5,41 @@ namespace k1fl1k\joyart\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use k1fl1k\joyart\Models\Artwork;
 use k1fl1k\joyart\Models\Tag;
+use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    public function showWelcome()
+    public function index(Request $request)
     {
+        $query = Artwork::where('is_published', true);
+
+        // Гість не бачить "questionable"
+        if (!auth()->check()) {
+            $query->where('rating', '!=', 'questionable');
+        }
+
+        if ($request->has('search') && $request->search) {
+            $query->whereHas('tags', function ($query) use ($request) {
+                $query->where('name', 'like', '%' . $request->search . '%');
+            });
+        }
+
+        // Фільтрація
+        $filter = $request->input('filter');
+        if ($filter === 'newest') {
+            $query->orderBy('created_at', 'desc');
+        } elseif ($filter === 'oldest') {
+            $query->orderBy('created_at', 'asc');
+        } elseif ($filter === 'image') {
+            $query->where('type', 'image');
+        } elseif ($filter === 'video') {
+            $query->where('type', 'video');
+        }
+
         $tags = Tag::whereNull('parent_id')->with('subtags')->get();
-        $images = Artwork::where('is_published', true)->paginate(50);
 
-        // Повертаємо вигляд і передаємо дані
+        $images = $query->paginate(50);
         return view('welcome', compact('tags', 'images'));
-
     }
 
     public function showProfile()
@@ -33,10 +57,5 @@ class WebController extends Controller
         })->orderBy('created_at', 'desc')->get();
 
         return view('profile', compact('user', 'userPosts', 'likedPosts'));
-    }
-
-    public function showSettings()
-    {
-        return view('settings');
     }
 }
