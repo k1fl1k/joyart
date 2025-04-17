5d2ad782ff918b577565813a0de6704429515a99
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-04-17 03:41:51 +0300

    Created route for updating classes.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace k1fl1k\joyart\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use k1fl1k\joyart\App\Livewire\Profile\UpdateAvatarForm;
+use k1fl1k\joyart\App\Livewire\Profile\UpdateBirthdayForm;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Livewire::component('profile.update-avatar-form', UpdateAvatarForm::class);
+        Livewire::component('profile.update-birthday-form', UpdateBirthdayForm::class);
     }
 }
