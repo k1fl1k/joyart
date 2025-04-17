f46730a01b6e4ebc5f29c2d4a2f9d14f53847ace
k1fl1k <c.heletii.yaroslav@student.uzhnu.edu.ua>
2025-04-17 03:44:22 +0300

    Created custom middleware.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     k1fl1k\joyart\Providers\AppServiceProvider::class,
     k1fl1k\joyart\Providers\MigrationServiceProvider::class,
+    k1fl1k\joyart\Providers\RouteServiceProvider::class,
     k1fl1k\joyart\Providers\SafebooruServiceProvider::class,
     k1fl1k\joyart\Providers\VoltServiceProvider::class,
 ];
